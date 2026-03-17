4274a84e8c5bb532d6e51a5f6a1d1de051df75bb
angushushu <angushushu@outlook.com>
2026-03-17 10:00:18 -0400

    up

--- types.ts
+++ types.ts
@@ -1,5 +1,15 @@
 import { InstrumentID } from './services/audioEngine';
 
+export interface KeyDef {
+  code: string;
+  label: string;
+  customLabel?: string;
+  width?: number;
+  height?: number;
+  isDummy?: boolean;
+  isModifier?: boolean;
+}
+
 export interface RecordedEvent {
   time: number;
   type: 'on' | 'off';
